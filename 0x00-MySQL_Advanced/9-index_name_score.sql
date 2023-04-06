f5a972e94ba0ac8cecceb16265c0344acd550537
kalibetre <kalbetre@icloud.com>
2023-04-07 00:15:28 +0300

    create index on name and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
--- SQL script that creates an index idx_name_first_score on the table names
--- and the first letter of name
-CREATE INDEX idx_name_first_score ON names (name(1));
+-- SQL script that creates an index idx_name_first on the table names
+-- on the first character of the name column and the score column
+CREATE INDEX idx_name_first_score ON names (name(1), score);
