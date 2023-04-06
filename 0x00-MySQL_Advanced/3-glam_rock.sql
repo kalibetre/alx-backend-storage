6d5060834da739e0495f43e119ed6a06c2fb845a
kalibetre <kalbetre@icloud.com>
2023-04-06 20:15:46 +0300

    use fixed year as 2020

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style
 -- ranked by thier longevity (number of years they have been active)
-SELECT band_name, COALESCE(split, YEAR(CURDATE())) - formed AS lifespan 
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan 
 FROM metal_bands 
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
