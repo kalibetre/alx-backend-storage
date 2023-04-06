a16691f6ec4d06b985128afa3b88a6f9e7e7474c
kalibetre <kalbetre@icloud.com>
2023-04-06 21:11:32 +0300

    using find_in_set

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style
 -- ranked by thier longevity (number of years they have been active)
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE FIND_IN_SET('Glam rock', style) > 0
 ORDER BY lifespan DESC;
