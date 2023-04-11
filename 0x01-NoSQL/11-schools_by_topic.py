1612e4886632e6fc3d78918ab7af0e162f376e6c
kalibetre <kalbetre@icloud.com>
2023-04-11 12:44:03 +0300

    find all matching docs

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 11-schools_by_topic module """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns the list of school having a specific topic
+    """
+    return list(
+        mongo_collection.find({"topics": {
+            "$elemMatch": {
+                "$eq": topic
+            }
+        }}))
